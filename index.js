41746d36400f70dadeb546e035b27adfaeaae9c1
Johnny Soong <johnnysoong@hotmail.com>
2018-07-24 18:50:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-let katzDeli = [];
+let katzDeliLine = [];
 
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, $(name). You are number &(katzDeliLine[katzDeliLine.length]) in line.`;
+}
+
+
+function nowServing() {
+  if (katzDeliLine.length === 0) return 'There is nobody waiting to be served';
+  else return `Currently serving $(katzDeliLine.shift()`;
+}
+
+function currentLine() {
+  let string = 'The line is currently: ';
+  for (let i = 0; i < katzDeliLine.length; i++) {
+    string += `$(i + 1). $(katzDeliLine[i]) `;
+  }
+  return string.slice(0, string[string.length - 2]);
+}
